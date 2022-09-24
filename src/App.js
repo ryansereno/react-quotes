f43318084fe35e0b958c5db3d40f2aeb942fcdeb
R <ryansereno@protonmail.com>
2022-09-24 12:45:56 -0400

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
+import React, { Suspense } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
-import AllQuotes from "./pages/AllQuotes";
-import QuoteDetail from "./pages/QuoteDetail";
-import NewQuote from "./pages/NewQuote";
-import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound";
 
-const quotes = [
-  { author: "ryan", text: "enjoy the moment", key: "1", id: "1" },
-];
+import Layout from "./components/layout/Layout";
+
+const NewQuote = React.lazy(() => {
+  return import("./pages/NewQuote");
+});
+const QuoteDetail = React.lazy(() => {
+  return import("./pages/QuoteDetail");
+});
+const NotFound = React.lazy(() => {
+  return import("./pages/NotFound");
+});
+const AllQuotes = React.lazy(() => {
+  return import("./pages/AllQuotes");
+});
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
+      <Suspense fallback={<p>Loading..</p>}>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
 
-        <Route path="/quotes" exact>
-          <AllQuotes quotes={quotes} />
-        </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
 
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
